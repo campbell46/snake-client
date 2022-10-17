0c3ae74e21f3be3dda65d3d607d97038eab68dd1
Sarah Campbell <sarahcampbell4646@gmail.com>
2022-10-17 14:27:50 -0400

    udated play.js

--- client.js
+++ client.js
@@ -1,6 +1,5 @@
 const net = require("net");
 
-
 const connect = function() {
   const conn = net.createConnection({
     host: "192.168.2.32",
@@ -22,12 +21,14 @@ const connect = function() {
   });
 
 
+
   conn.on("data", () => {
     console.log("you ded cuz you idled");
   });
 
   return conn;
 };
+
 
 module.exports = {
   connect
