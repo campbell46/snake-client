b866383b26bc5b47747a160dc88b535b0446459a
Sarah Campbell <sarahcampbell4646@gmail.com>
2022-10-17 13:34:59 -0400

    updated client.js

--- client.js
+++ client.js
@@ -1,17 +1,26 @@
 const net = require("net");
 
+
 const connect = function() {
   const conn = net.createConnection({
     host: "192.168.2.32",
     port: 50541,
   });
 
+  // const myTimeout = setTimeout(conn.write("Move: left"), 5000);
+
   conn.setEncoding("utf8");
+
+  // const myTimeout = setTimeout(conn.write("Move: left"), 50);
 
   conn.on("connect", () => {
     console.log("Successfully connected to game server");
     conn.write("Name: SAC");
+    // setInterval(() => {
+    //   conn.write("Move: up");
+    // }, 50);
   });
+
 
   conn.on("data", () => {
     console.log("you ded cuz you idled");
