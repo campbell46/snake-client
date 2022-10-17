9e3f484acb9390697847b669f9cd95dfe0a7f170
Sarah Campbell <sarahcampbell4646@gmail.com>
2022-10-17 14:33:40 -0400

    created input module, required in play.js

--- play.js
+++ play.js
@@ -1,24 +1,8 @@
-const { stdin } = require("process");
+// const { stdin } = require("process");
 const { connect } = require("./client");
+const { setupInput } = require("./input");
 
 console.log("Connecting...");
 connect();
-
-const setupInput = function() {
-  const stdin = process.stdin;
-  stdin.setRawMode(true);
-  stdin.setEncoding("utf8");
-  stdin.resume();
-
-  stdin.on("data", handleUserInput);
-
-  return stdin;
-};
-
-const handleUserInput = (data) => {
-  if (data === '\u0003') {
-    process.exit;
-  }
-};
 
 setupInput;
