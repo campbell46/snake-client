219ba63fd74a26442413ac8e51fae138d60ecd3b
Sarah Campbell <sarahcampbell4646@gmail.com>
2022-10-17 12:40:55 -0400

    updated client.js

--- client.js
+++ client.js
@@ -8,6 +8,11 @@ const connect = function() {
 
   conn.setEncoding("utf8");
 
+  conn.on("connect", () => {
+    console.log("Successfully connected to game server");
+    conn.write("Name: SAC");
+  });
+
   conn.on("data", () => {
     console.log("you ded cuz you idled");
   });
