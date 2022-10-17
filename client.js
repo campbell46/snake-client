f3ad351bb157a64dc87f822e55bdcf2c83b531a2
Sarah Campbell <sarahcampbell4646@gmail.com>
2022-10-17 16:28:54 -0400

    added constants file

--- client.js
+++ client.js
@@ -1,23 +1,17 @@
 const net = require("net");
+const { IP, PORT } = require("./constants");
 
 const connect = function() {
   const conn = net.createConnection({
-    host: "192.168.2.32",
-    port: 50541,
+    host: IP,
+    port: PORT,
   });
 
-  // const myTimeout = setTimeout(conn.write("Move: left"), 5000);
-
   conn.setEncoding("utf8");
-
-  // const myTimeout = setTimeout(conn.write("Move: left"), 50);
 
   conn.on("connect", () => {
     console.log("Successfully connected to game server");
     conn.write("Name: SAC");
-    // setInterval(() => {
-    //   conn.write("Move: up");
-    // }, 50);
   });
 
 
---
+++ constants.js
@@ -0,0 +1,7 @@
+const IP = "192.168.2.32";
+const PORT = 50541;
+
+module.exports = {
+  IP,
+  PORT,
+};
